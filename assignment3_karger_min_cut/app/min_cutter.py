018a8923c6f24216bdcc34b060e5e625b06615ea
Mike Milonakis <michail.milonakis@gmail.com>
2016-10-01 16:40:30 +0300

    Finish karger's algorithm

--- assignment3_karger_min_cut/app/min_cutter.py
+++ assignment3_karger_min_cut/app/min_cutter.py
@@ -1,15 +1,39 @@
+from random import randint
 
-class MinCutter:
+
+class KargerMinCutter:
     def __init__(self, graph_file):
-
+        self._graph = {}
+        self._total_edges = 0
         with open(graph_file) as file:
-            self._graph = {}
             for index, line in enumerate(file):
-                numbers = [number for number in map(int, line.split())]
+                numbers = [int(number) for number in line.split()]
                 self._graph[numbers[0]] = numbers[1:]
-        pass
+                self._total_edges += len(numbers[1:])
 
-    def get_graph(self):
-        print(self._graph)
-        return self._graph
+    def find_min_cut(self):
+        min_cut = 0
+        while len(self._graph) > 2:
+            v1, v2 = self._pick_random_edge()
+            self._total_edges -= len(self._graph[v1])
+            self._total_edges -= len(self._graph[v2])
+            self._graph[v1].extend(self._graph[v2])
+            for vertex in self._graph[v2]:
+                self._graph[vertex].remove(v2)
+                self._graph[vertex].append(v1)
+            self._graph[v1] = list(filter(lambda v: v != v1, self._graph[v1]))
+            self._total_edges += len(self._graph[v1])
+            self._graph.pop(v2)
+        for edges in self._graph.values():
+            min_cut = len(edges)
+        return min_cut
 
+    def _pick_random_edge(self):
+        rand_edge = randint(0, self._total_edges - 1)
+        for vertex, vertex_edges in self._graph.items():
+            if len(vertex_edges) <= rand_edge:
+                rand_edge -= len(vertex_edges)
+            else:
+                from_vertex = vertex
+                to_vertex = vertex_edges[rand_edge]
+                return from_vertex, to_vertex
--- assignment3_karger_min_cut/test/test_min_cutter.py
+++ assignment3_karger_min_cut/test/test_min_cutter.py
@@ -1,10 +1,48 @@
 import unittest
-from assignment3_karger_min_cut.app.min_cutter import  MinCutter
+from assignment3_karger_min_cut.app.min_cutter import KargerMinCutter
 
 
 class MinCutterTest(unittest.TestCase):
-    def test_min_cutter_reads_graph(self):
-        min_cutter = MinCutter('four_nodes_test.txt')
-        vertexes = min_cutter.get_graph()
-        self.assertIsNotNone(vertexes)
 
+    def setUp(self):
+        self._min_cutter = KargerMinCutter('four_nodes_test.txt')
+
+    def test_picks_random_edge(self):
+        v1, v2 = self._min_cutter._pick_random_edge()
+        self.assertIsNotNone((v1, v2))
+
+    def test_eventually_finds_min_cut_4nodes(self):
+        min_cut = 999
+        for i in range(8):
+            min_cutter = KargerMinCutter('four_nodes_test.txt')
+            cut = min_cutter.find_min_cut()
+            if cut < min_cut:
+                min_cut = cut
+        self.assertEqual(min_cut, 2)
+
+    def test_eventually_finds_min_cut_8nodes(self):
+        min_cut = 999
+        for i in range(16):
+            min_cutter = KargerMinCutter('eight_nodes_test.txt')
+            cut = min_cutter.find_min_cut()
+            if cut < min_cut:
+                min_cut = cut
+        self.assertEqual(min_cut, 2)
+
+    def test_eventually_finds_min_cut_40nodes(self):
+        min_cut = 999
+        for i in range(80):
+            min_cutter = KargerMinCutter('40_nodes_test.txt')
+            cut = min_cutter.find_min_cut()
+            if cut < min_cut:
+                min_cut = cut
+        self.assertEqual(min_cut, 3)
+
+    def test_eventually_finds_min_cut_40nodes(self):
+        min_cut = 99999
+        for i in range(40000):
+            min_cutter = KargerMinCutter('coursera_test.txt')
+            cut = min_cutter.find_min_cut()
+            if cut < min_cut:
+                min_cut = cut
+            print(min_cut)
