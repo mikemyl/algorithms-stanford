94d72625a2e7c1da2264f9a5b2644898fc2484dd
sestus <michail.milonakis@gmail.com>
2016-10-08 20:12:15 +0300

    Add 5th assignment - dijkstra

--- assignment5_dijkstra/app/dijkstra_path_finder.py
+++ assignment5_dijkstra/app/dijkstra_path_finder.py
@@ -8,5 +8,30 @@ class DijkstraPathFinder:
         with open(input_file) as file:
             for line in file:
                 line_content = line.split()
-                self.graph[line_content[0]] = [literal_eval(edge) for edge in line_content[1:]]
-        print(self.graph)
+                self.graph[int(line_content[0])] = [literal_eval(edge) for edge in line_content[1:]]
+        self._source_vertex = next(iter(self.graph.keys()))
+
+    def compute_shortest_paths(self, source=None):
+        if source is None:
+            source = self._source_vertex
+        shortest_paths = {}
+        visited = set()
+        for vertex in self.graph.keys():
+            shortest_paths[vertex] = (9999999999, [])
+        shortest_paths[source] = (0, [])
+        visited.add(source)
+        while set(self.graph.keys() - visited):
+            source, min_edge = -1, ()
+            for vertex in visited:
+                for edge in self.graph[vertex]:
+                    if edge[0] in visited:
+                        continue
+                    if not min_edge or shortest_paths[vertex][0] + edge[1] < min_edge[1]:
+                        min_edge = (edge[0], shortest_paths[vertex][0] + edge[1])
+                        source = vertex
+            shortest_paths[min_edge[0]] = (min_edge[1], shortest_paths[source][1] + [min_edge[0]])
+            visited.add(min_edge[0])
+        return shortest_paths
+
+
+
--- assignment5_dijkstra/test/test_dijkstra_path_finder.py
+++ assignment5_dijkstra/test/test_dijkstra_path_finder.py
@@ -5,7 +5,32 @@ from assignment5_dijkstra.app.dijkstra_path_finder import DijkstraPathFinder
 class MyTestCase(unittest.TestCase):
     def test_small_graph(self):
         path_finder = DijkstraPathFinder('forum_1.txt')
+        paths = path_finder.compute_shortest_paths()
+        expected = {1: (0, []), 2: (1, [2]), 3: (2, [2, 3]), 4: (3, [2, 3, 4]), 5: (4, [2, 3, 4, 5]), 6: (4, [8, 7, 6])
+                    , 7: (3, [8, 7]), 8: (2, [8])}
+        self.assertEqual(expected, paths)
 
+    def test_small_graph_2(self):
+        path_finder = DijkstraPathFinder('forum_2.txt')
+        paths = path_finder.compute_shortest_paths()
+        expected = {1: 0, 3: 2, 2: 3, 6: 4, 7: 4, 10: 4, 4: 6, 8: 6, 9: 8, 5: 9}
+        actual = {vertex: distance[0] for (vertex, distance) in paths.items()}
+        self.assertEqual(expected, actual)
+
+    def test_assignment_5(self):
+        path_finder = DijkstraPathFinder('assignment_5.txt')
+        paths = path_finder.compute_shortest_paths()
+        actual = {vertex: distance[0] for (vertex, distance) in paths.items()}
+        print(actual[7])
+        print(actual[37])
+        print(actual[59])
+        print(actual[82])
+        print(actual[99])
+        print(actual[115])
+        print(actual[133])
+        print(actual[165])
+        print(actual[188])
+        print(actual[197])
 
 if __name__ == '__main__':
     unittest.main()
