26ccaa2e2c196385e90c064926935d57768c162c
sestus <michail.milonakis@gmail.com>
2016-11-07 20:54:59 +0200

    Initial prim algorithm steps

--- part_2/assignment1_greedy_algorithms/app/prim_mst.py
+++ part_2/assignment1_greedy_algorithms/app/prim_mst.py
@@ -1,2 +1,22 @@
 class PrimMST:
-    pass
+# 4, 16, -3
+    def __init__(self, graph_file):
+        self._graph = {}
+        with open(graph_file, "r") as file:
+            num_vertices, num_edges = (int(num) for num in file.readline().split())
+            for i in range(1, num_vertices + 1):
+                self._graph[i] = []
+            for i in range(num_edges):
+                from_vertex, to_vertex, weight = (int(num) for num in file.readline().split())
+                self._graph[from_vertex].append((to_vertex, weight))
+        self._compute_mst()
+
+    def _compute_mst(self):
+        unvisited = set(self._graph.keys())
+        visited = set()
+        visited.add(1)
+        unvisited.remove(1)
+
+
+if __name__ == "__main__":
+    prim = PrimMST("assignment_1.2.txt")
